254f7ce41959b5fb65843f080e15be20c13a0a9d
RafaelCherreraco <rafael@cherrera.co>
2021-08-31 15:17:40 -0500

    Commit v1.0

---
+++ contracts/Weth.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.7;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Weth is ERC20 {
+    constructor() ERC20("Wrapped Ether", "WETH") {}
+
+    function deposit() external payable {
+        _mint(msg.sender, msg.value);
+    }
+
+    function withdraw(uint amount) external {
+        require(balanceOf(msg.sender) >= amount, "Balance too low");
+        _burn(msg.sender, amount);
+        msg.sender.transfer(amount);
+    }
+}
